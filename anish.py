2de5e89c0171c2a96ab5da03e0199e2069e145a0
Kumar Aklavya <119126485+aklavya-07@users.noreply.github.com>
2022-12-04 14:54:54 +0530

    Add files via upload

---
+++ anish.py
@@ -0,0 +1,11 @@
+Eng=eval(input("enter the  marks of english:"))
+phy=eval(input("enter the marks of Physics:"))
+chem=eval(input("enter the marks of Chemistry:"))
+maths=eval(input("enter the marks of Maths:"))
+sum=Eng+phy+chem+maths
+percentage=(sum/400)*100
+if (percentage<=80):
+     print("you are scoring grade A")
+percentage=(sum/400)*100
+if (percentage<=60):
+      print("you are scoring grade A")
